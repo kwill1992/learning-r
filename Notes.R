c3b5225763fd9f96971d378976446d5d0c911b3d
Kevin Williams <44032970+kwill1992@users.noreply.github.com>
2022-01-17 15:43:00 -0500

    Updated notes.

---
+++ Notes.R
@@ -0,0 +1,23 @@
+# Need to make this into a RMarkDown file
+# Add keywords
+
+
+#save and load datasets in R
+# http://rcrastinate.rbind.io/post/how-to-save-and-load-data-in-r-an-overview/
+# https://www.r-bloggers.com/2019/05/how-to-save-and-load-datasets-in-r-an-overview/
+# saveRDS(mnist, file = "mnistRDS.Rds") keeps the R data structure for immediate reload.
+
+# Git and R
+https://happygitwithr.com/rstudio-git-github.html
+
+# R packages if not installed
+https://vbaliga.github.io/verify-that-r-packages-are-installed-and-loaded/
+  # This one below is best
+https://statsandr.com/blog/an-efficient-way-to-install-and-load-r-packages/
+https://statisticsglobe.com/r-install-missing-packages-automatically
+https://r-coder.com/install-r-packages/
+  
+  
+  # Packages, install source or binary.
+  # for the most, just do binary.
+  https://community.rstudio.com/t/meaning-of-common-message-when-install-a-package-there-are-binary-versions-available-but-the-source-versions-are-later/2431
